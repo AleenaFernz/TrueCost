408b0ac7f2fdf23c82db57835611c3f8cfa98fed
AleenaFernz <aleena.fernz@gmail.com>
2025-10-04 12:15:00 +0530

    Database connetion

---
+++ server/src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get('/', (req, res) => res.send('Backend running successfully 🚀'));
+
+export default app;
